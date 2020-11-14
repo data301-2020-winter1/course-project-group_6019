5801424ab07cdbe6bd7ba581c78b6c1058b7a336
jeran knorr <jeranknorr@jerans-MacBook-Air.local>
2020-11-14 11:07:00 -0800

    milestone 2 update

---
+++ analysis/jeranknorr/scripts.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+def load_and_process(Madden21_Clean):
+    df= (
+        pd.read_csv('../../data/raw/madden21_ratings.csv')
+        .dropna()
+    )
+   
+    df2= (    
+            df.filter(['Team', 'Full Name', 'Overall Rating', 'Position'])
+)
+    return df2
